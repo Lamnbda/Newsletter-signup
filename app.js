dc299a55c38f9b9398ce8c2e55024b8851bfaee1
VincentLabor <vlabor95@gmail.com>
2019-05-09 00:40:07 -0700

    Basic signup page and some node.js added

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var request = require('request');
 
 var app = express();
 
-app.get("/", function (req, res) {
-res.sendFile(__dirname + "/signup.html");
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/signup.html");
 })
 
 app.listen(3000, function () {
