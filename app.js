a85dfd03d0306e78b0d59244d10ed850ec955773
VincentLabor <vlabor95@gmail.com>
2019-05-09 00:11:36 -0700

    Necessary modules added

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 var express = require('express');
 var bodyparser = require('body-parser');
+var request = require('request');
 
+var app = express();
+
+app.get("/", function (req, res) {
+res.sendFile(__dirname + "/signup.html");
+})
+
+app.listen(3000, function () {
+    console.log("The server is online")
+});
