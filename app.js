0a268bcddc8a6660a186f02993a857a9fc241f6c
VincentLabor <vlabor95@gmail.com>
2019-05-10 01:19:13 -0700

    Added post method and can record User credentials

--- app.js
+++ app.js
@@ -4,12 +4,26 @@ var request = require('request');
 
 var app = express();
 
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", function(req,res){
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/signup.html");
 });
+
+
+
+app.post('/', function (req, res) {
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var email = req.body.email;
+console.log(firstName, lastName, email)
+})
 
 app.listen(3000, function () {
     console.log("The server is online")
 });
+
+//a06d629d471a7dbb668471561fb9f2d6-us20
